5a12603eb334ac1b0c54102a19f9a582ca38aa09
Thomas Christensen <thomas@chrstnsn.dk>
2012-06-25 11:22:15 +0200

    initial

---
+++ config/bindings.el
@@ -0,0 +1,10 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+(custom-set-variables
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta))
+ '(ns-function-modifier (quote none)))
