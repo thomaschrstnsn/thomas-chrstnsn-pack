e8ffc1cade8e1d8407e6f35e40ccda89d7d80750
Thomas Christensen <thomas@chrstnsn.dk>
2013-05-15 20:45:30 +0200

    sync git-gutter background with solarized-dark

--- config/gui.el
+++ config/gui.el
@@ -1,3 +1,5 @@
 (add-to-list 'default-frame-alist '(font . "PragmataPro-18"))
 (add-to-list 'default-frame-alist '(height . 45))
 (add-to-list 'default-frame-alist '(width . 135))
+
+(set-face-background 'git-gutter:unchanged "#073642")
