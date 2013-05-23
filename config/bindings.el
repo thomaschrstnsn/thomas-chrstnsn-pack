e3e382f99806e11248c001299bfdf0ef60cd312d
Thomas Christensen <thomas@chrstnsn.dk>
2013-05-23 21:25:04 +0200

    duplicate s-exp (via bodil)

--- config/bindings.el
+++ config/bindings.el
@@ -25,3 +25,29 @@
 (global-set-key (kbd "M-p") 'smart-symbol-go-backward)
 
 (global-set-key (kbd "C-c C-n") 'rename-current-buffer-file)
+
+;; Duplicate sexp
+(defun paredit-duplicate-after-point
+  ()
+  "Duplicates the content of the line that is after the point."
+  (interactive)
+  ;; skips to the next sexp
+  (while (looking-at " ")
+    (forward-char))
+  (set-mark-command nil)
+  ;; while we find sexps we move forward on the line
+  (while (and (<= (point) (car (bounds-of-thing-at-point 'sexp)))
+              (not (= (point) (line-end-position))))
+    (forward-sexp)
+    (while (looking-at " ")
+      (forward-char)))
+  (kill-ring-save (mark) (point))
+  ;; go to the next line and copy the sexprs we encountered
+  (paredit-newline)
+  (set-mark-command nil)
+  (yank)
+  (exchange-point-and-mark))
+
+(eval-after-load "paredit"
+  '(define-key paredit-mode-map (kbd "C-c C-d")
+     'paredit-duplicate-after-point))
