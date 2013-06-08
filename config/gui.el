59a0612ed6593cad4367756e7c3cce983c94c37a
Thomas Christensen <thomas@chrstnsn.dk>
2013-06-08 20:01:15 +0200

    tweaks to ui

--- config/gui.el
+++ config/gui.el
@@ -3,3 +3,9 @@
 (add-to-list 'default-frame-alist '(width . 135))
 
 (set-face-background 'git-gutter:unchanged "#073642")
+
+; highlight current line, always
+(global-hl-line-mode 1)
+
+; show full path to current file
+(setq frame-title-format '(buffer-file-name "%f" ("%b")))
