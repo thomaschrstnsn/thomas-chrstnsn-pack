794ca55ccaec8020f34cb1b0f13416490d9cf12c
Thomas Christensen <thomas@chrstnsn.dk>
2013-06-21 16:28:52 +0200

    eval and replace elisp-expression in buffer

--- config/bindings.el
+++ config/bindings.el
@@ -55,3 +55,15 @@
 (eval-after-load "paredit"
   '(define-key paredit-mode-map (kbd "C-c C-d")
      'paredit-duplicate-after-point))
+
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+(global-set-key (kbd "C-c e") 'eval-and-replace)
