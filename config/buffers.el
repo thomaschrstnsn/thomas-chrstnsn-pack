8a1ad5a599495dc23af2aea9f1abfe7736904cf6
Thomas Christensen <thomas@chrstnsn.dk>
2013-06-24 23:02:16 +0200

    remove unused ido/etags stuff

--- config/buffers.el
+++ config/buffers.el
@@ -23,29 +23,6 @@
 
 (setq visible-bell t)
 
-(require 'etags)
-
-(defun ido-find-tag ()
-  "Find a tag using ido"
-  (interactive)
-  (tags-completion-table)
-  (let (tag-names)
-    (mapc (lambda (x)
-            (unless (integerp x)
-              (push (prin1-to-string x t) tag-names)))
-          tags-completion-table)
-    (find-tag (ido-completing-read "Tag: " tag-names))))
-
-(defun ido-find-file-in-tag-files ()
-  (interactive)
-  (save-excursion
-    (let ((enable-recursive-minibuffers t))
-      (visit-tags-table-buffer))
-    (find-file
-     (expand-file-name
-      (ido-completing-read
-       "Project file: " (tags-table-files) nil t)))))
-
 ;; to improve the movement in files
 
 (defvar smart-use-extended-syntax nil
