9575956e49cc88953c9c679ef53582ab299e2e3f
Thomas Christensen <thomas@chrstnsn.dk>
2014-01-27 22:14:50 +0100

    use diminish to clean mode-line

--- config/clean-mode-line.el
+++ config/clean-mode-line.el
@@ -1,41 +1,21 @@
-;; based on http://blog.jr0cket.co.uk/2013/01/tweeking-emacs-modeline-for-clojure.html
+(live-add-pack-lib "diminish")
 
-(defvar mode-line-cleaner-alist
-  `((auto-complete-mode . " α")
-    (yas-minor-mode . " γ")
-    (paredit-mode . " Φ")
-    (eldoc-mode . "")
-    (abbrev-mode . "")
-    (undo-tree-mode . " τ")
-    (volatile-highlights-mode . " υ")
-    (elisp-slime-nav-mode . " δ")
-    (nrepl-mode . " η")
-    (nrepl-interaction-mode . " η")
-    ;; Major modes
-    (clojure-mode . "λ")
-    (hi-lock-mode . "")
-    (python-mode . "")
-    (emacs-lisp-mode . "ε")
-    (markdown-mode . "md"))
-  "Alist for `clean-mode-line'.
+;; Unclutter the modeline
+(require 'diminish)
+(eval-after-load "yasnippet" '(diminish 'yas-minor-mode))
+(eval-after-load "eldoc" '(diminish 'eldoc-mode))
+(eval-after-load "paredit" '(diminish 'paredit-mode))
+(eval-after-load "undo-tree" '(diminish 'undo-tree-mode))
+(eval-after-load "auto-complete" '(diminish 'auto-complete-mode))
+(eval-after-load "volatile-highlights" '(diminish 'volatile-highlights-mode))
+(eval-after-load "elisp-slime-nav" '(diminish 'elisp-slime-nav-mode))
+(eval-after-load "smartparens" '(diminish 'smartparens-mode))
+(eval-after-load "guide-key" '(diminish 'guide-key-mode))
+(eval-after-load "git-gutter" (diminish 'git-gutter-mode))
 
-When you add a new element to the alist, keep in mind that you
-must pass the correct minor/major mode symbol and a string you
-want to use in the modeline *in lieu of* the original.")
+(defmacro rename-modeline (package-name mode new-name)
+  `(eval-after-load ,package-name
+     '(defadvice ,mode (after rename-modeline activate)
+        (setq mode-name ,new-name))))
 
-(defun clean-mode-line ()
-  (interactive)
-  (loop for cleaner in mode-line-cleaner-alist
-        do (let* ((mode (car cleaner))
-                 (mode-str (cdr cleaner))
-                 (old-mode-str (cdr (assq mode minor-mode-alist))))
-             (when old-mode-str
-                 (setcar old-mode-str mode-str))
-               ;; major mode
-             (when (eq mode major-mode)
-               (setq mode-name mode-str)))))
-
-(add-hook 'after-change-major-mode-hook 'clean-mode-line)
-
-;;; Greek letters - C-u C-\ greek ;; C-\ to revert to default
-;;; ς ε ρ τ υ θ ι ο π α σ δ φ γ η ξ κ λ ζ χ ψ ω β ν μ
+(rename-modeline "clojure-mode" clojure-mode "clj")
