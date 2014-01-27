c27a1d4d5fd54576eb6f0ff0e579227effb1d7c1
Thomas Christensen <thomas@chrstnsn.dk>
2014-01-27 22:15:20 +0100

    no blinking cursor and relayout

--- config/gui.el
+++ config/gui.el
@@ -1,9 +1,21 @@
-(add-to-list 'default-frame-alist '(font . "PragmataPro-18"))
-(add-to-list 'default-frame-alist '(height . 45))
-(add-to-list 'default-frame-alist '(width . 135))
-
 ; highlight current line, always
 (global-hl-line-mode 1)
 
-; show full path to current file
+;; Don't defer screen updates when performing operations
+(setq redisplay-dont-pause t)
+
+;; no blinkies
+(blink-cursor-mode -1)
+
+;; ma' font
+(add-to-list 'default-frame-alist '(font . "PragmataPro-19"))
+
+;; default frame sizes
+(add-to-list 'default-frame-alist '(height . 45))
+(add-to-list 'default-frame-alist '(width . 135))
+
+;; no tooltips
+(tooltip-mode -1)
+
+;; show full path to current file
 (setq frame-title-format '(buffer-file-name "%f" ("%b")))
