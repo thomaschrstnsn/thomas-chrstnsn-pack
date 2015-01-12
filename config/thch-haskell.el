9aa9e8590fa9e32f0e7513db152657897b03ba94
Thomas Christensen <thomas@chrstnsn.dk>
2015-01-12 21:30:38 +0100

    still broken (less than perfect) haskell support

--- config/thch-haskell.el
+++ config/thch-haskell.el
@@ -1,3 +1,13 @@
 (setq haskell-font-lock-symbols 'unicode)
 
+;; -- UNFINISHED Haskell support
+;; (prelude-require-packages '(haskell-mode ghc))
+;; (prelude-require-packages '(haskell-mode ghc ghci-completion company-ghc))
+;; (require 'haskell-session)
+;; (require 'haskell-indentation)
+
+;; (autoload 'ghc-init "ghc" nil t)
+;; (autoload 'ghc-debug "ghc" nil t)
+;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
 (provide 'thch-haskell)
