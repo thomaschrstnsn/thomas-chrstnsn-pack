9f7e5266c1fe76710a3207f33d4a6f8d4cd7f53a
Thomas Christensen <thomas@chrstnsn.dk>
2015-04-18 20:49:39 +0200

    haskell mode improvements

--- config/thch-haskell.el
+++ config/thch-haskell.el
@@ -1,13 +1,42 @@
 (setq haskell-font-lock-symbols 'unicode)
 
-;; -- UNFINISHED Haskell support
-;; (prelude-require-packages '(haskell-mode ghc))
-;; (prelude-require-packages '(haskell-mode ghc ghci-completion company-ghc))
-;; (require 'haskell-session)
-;; (require 'haskell-indentation)
+;; I comment out the line:
+;;     (cons "()" (decode-char 'ucs #X2205))
+;; in haskell-font-lock.el
+;; TODO: figure out how to do the above programatically
 
-;; (autoload 'ghc-init "ghc" nil t)
-;; (autoload 'ghc-debug "ghc" nil t)
-;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(prelude-require-packages '(hi2 hindent))
+;; better haskell indents
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
+;; nice indents of current decl using M-q, requires hindent installed in cabal
+;; (add-hook 'haskell-mode-hook #'hindent-mode)
+;; (setq hindent-style "johan-tibell")
+
+;; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+
+;; cabal installs:
+;;  hindent
+;;  hasktags
+;;  ghc-mod
+;;  structured-haskell-mode
+
+;;;; Structured Haskell Mode (SHM)
+;; Currently shm is not available on melpa, so...
+;; Clone the project from github:
+;; $ git clone https://github.com/chrisdone/structured-haskell-mode.git
+;; You need to install the structured-haskell-mode executable which does the parsing.
+
+;; $ cd structured-haskell-mode
+;; $ cabal install
+;; $ cd elisp/
+;; $ make
+;; Add the elisp library to your load-path and require the library.
+
+;; Enable it by uncommenting the two lines below:
+;; (add-to-list 'load-path "/Users/thomas/src/structured-haskell-mode/elisp")
+;; (require 'shm)
+
+;; hasktags, M-. goto definition in project
+(custom-set-variables '(haskell-tags-on-save t))
 
 (provide 'thch-haskell)
