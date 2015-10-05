fb876d44737cb72c55a46b2d3565b8ec0013c781
Thomas Christensen <thomas@chrstnsn.dk>
2015-10-05 21:35:26 +0200

    ok - so apparently ghci-ng is where it is at

--- config/thch-haskell.el
+++ config/thch-haskell.el
@@ -3,24 +3,28 @@
 ;; remove irritating symbol expansion
 (setq haskell-font-lock-symbols-alist (remove '("()" . "∅") haskell-font-lock-symbols-alist))
 
-(prelude-require-packages '(hi2 hindent ghc))
+(prelude-require-packages '(hi2 hindent))
+
 ;; better haskell indents
 (add-hook 'haskell-mode-hook 'turn-on-hi2)
 
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(add-hook 'haskell-mode-hook
+          (lambda ()
+            ;; install (from source): https://github.com/chrisdone/ghci-ng
+            (setq haskell-process-type 'cabal-repl)
+            (setq haskell-process-args-cabal-repl
+                  '("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng"))
+
+            (define-key interactive-haskell-mode-map (kbd "M-.") 'haskell-mode-goto-loc)
+            (define-key interactive-haskell-mode-map (kbd "M-,") 'xref-pop-marker-stack)
+            (define-key interactive-haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
+            (define-key interactive-haskell-mode-map (kbd "C-c C-u") 'haskell-mode-find-uses)))
 
 ;; nice indents of current decl using M-q, requires hindent installed in cabal
-;; (add-hook 'haskell-mode-hook #'hindent-mode)
+;;(add-hook 'haskell-mode-hook #'hindent-mode)
 ;; (setq hindent-style "johan-tibell")
 
 ;; cabal installs:
 ;;  hindent
-;;  hasktags
-;;  ghc-mod
-
-;; hasktags, M-. goto definition in project
-(custom-set-variables '(haskell-tags-on-save t))
 
 (provide 'thch-haskell)
