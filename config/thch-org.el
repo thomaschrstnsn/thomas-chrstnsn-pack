6a8b38f4d38ad5e81d21a34fb5069acac6c68fd9
Thomas Christensen <thomas@chrstnsn.dk>
2016-01-07 14:32:18 +0100

    org-mode tweaks
    no mobileorg and no logging complete, added waiting state

--- config/thch-org.el
+++ config/thch-org.el
@@ -5,24 +5,14 @@
 (setq org-directory "~/org")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 
-;; MobileOrg
-;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull "~/org/flagged.org")
-;; Set to <your Dropbox root directory>/MobileOrg.
-(if (file-exists-p "~/Desktop/Dropbox/Apps/MobileOrg")
-    (setq org-mobile-directory "~/Desktop/Dropbox/Apps/MobileOrg"))
-(if (file-exists-p "~/Dropbox/Apps/MobileOrg")
-    (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg"))
-
-(setq org-log-done 'time)
-
 (setq org-todo-keywords
-      '((sequence "TODO" "IN-PROGRESS" "VERIFICATION" "|"  "DONE" "FAIL" "DELEGATED" "CANCELLED")))
+      '((sequence "TODO" "IN-PROGRESS" "VERIFICATION" "WAITING" "|"  "DONE" "FAIL" "DELEGATED" "CANCELLED")))
 
 (setq org-todo-keyword-faces
       '(("TODO"  . (:foreground "firebrick2" :weight bold))
         ("IN-PROGRESS"  . (:foreground "olivedrab" :weight bold))
         ("VERIFICATION"  . (:foreground "sienna" :weight bold))
+        ("WAITING"  . (:foreground "dimgrey" :weight italic))
         ("DONE"  . (:foreground "forestgreen" :weight bold))
         ("DELEGATED"  . (:foreground "dimgrey" :weight bold))
         ("FAILED"  . (:foreground "steelblue" :weight bold))
